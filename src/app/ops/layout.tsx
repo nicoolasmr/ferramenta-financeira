e322c32013130c7f443cf3c1ea09710793148c93
Nicolas Moreira <nicoolascf5@gmail.com>
2026-01-22 16:42:10 -0300

    fix: wrap ops layout in OrganizationProvider to fix build error

--- src/app/ops/layout.tsx
+++ src/app/ops/layout.tsx
@@ -1,5 +1,8 @@
 import { Sidebar } from "@/components/layout/sidebar";
 import { Header } from "@/components/layout/header";
+import { OrganizationProvider } from "@/components/providers/OrganizationProvider";
+import { Toaster } from "sonner";
+import { ErrorBoundary } from "@/components/ErrorBoundary";
 
 export default function OpsLayout({
     children,
@@ -7,14 +10,19 @@ export default function OpsLayout({
     children: React.ReactNode;
 }) {
     return (
-        <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
-            <Sidebar />
-            <div className="flex flex-col">
-                <Header />
-                <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6 bg-slate-50">
-                    {children}
-                </main>
+        <OrganizationProvider>
+            <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
+                <Toaster position="top-right" richColors />
+                <Sidebar />
+                <div className="flex flex-col">
+                    <Header />
+                    <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6 bg-slate-50">
+                        <ErrorBoundary>
+                            {children}
+                        </ErrorBoundary>
+                    </main>
+                </div>
             </div>
-        </div>
+        </OrganizationProvider>
     );
 }
